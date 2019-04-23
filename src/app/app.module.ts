9352d4053d35acd8030530e6bb539c2c2c1715aa
Ricardo Lira <ricardo.19.rl09@gmail.com>
2019-04-23 09:38:43 -0500

    Modificaciones en modulos y creacion de rutas Hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,8 @@ import { AppComponent } from './app.component';
 import { appRouting } from './app.routes';
 
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
+import { PagesModule } from './pages/pages.modulo';
 
 
 
@@ -22,19 +14,14 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+  
   ],
   imports: [
     BrowserModule,
-    appRouting
+    appRouting,
+    PagesModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
